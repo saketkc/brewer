b165b74c59c5385ce6c1cd4e0a84acccef061ffd
Saket Choudhary <saketkc@gmail.com>
2014-11-22 00:45:21 -0800

    Check if linuxbrew works on recursive

--- methpipe.rb
+++ methpipe.rb
@@ -1,17 +1,17 @@
 require "formula"
 
 class Methpipe < Formula
-  head "https://github.com/smithlabcode/methpipe.git"
   homepage "http://smithlabresearch.org/software/methpipe/"
   url "http://smithlabresearch.org/downloads/methpipe-3.3.1.tar.gz"
   sha1 "00dea4a51e7b8ed5e7020bba89db63a91d86a70f"
+  head "https://github.com/smithlabcode/methpipe.git"
 
   depends_on "gsl" => :build
 
   def install
     system "make", "all"
     system "make", "install"
-    prefix.install 'bin'
+    prefix.install "bin"
   end
 
   test do
