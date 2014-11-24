13e1917389805a2a36eadcc69663bdbb6f8f86e7
Saket Choudhary <saketkc@gmail.com>
2014-11-24 00:54:16 -0800

    Add doi, tag

--- methpipe.rb
+++ methpipe.rb
@@ -3,6 +3,9 @@ require "formula"
 class Methpipe < Formula
   head "https://github.com/smithlabcode/methpipe.git"
   homepage "http://smithlabresearch.org/software/methpipe/"
+  #tag "bioinformatics"
+  #doi "10.1371/journal.pone.0081148"
+  
   url "http://smithlabresearch.org/downloads/methpipe-3.3.1.tar.gz"
   sha1 "00dea4a51e7b8ed5e7020bba89db63a91d86a70f"
 
