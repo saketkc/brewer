e554a4cf362e639b5045d3e1ed1f8d7c7ca492b2
Saket Choudhary <saketkc@gmail.com>
2015-01-12 19:19:30 -0800

    travisCI: More verbose
    Signed-off-by: Saket Choudhary <saketkc@gmail.com>

--- travisCI/travis.sh
+++ travisCI/travis.sh
@@ -18,33 +18,27 @@ error_handler() {
   dump_output
   exit 1
 }
-# If an error occurs, run our error handler to output a tail of the build
+# If an error occurs\  run our error handler to output a tail of the build
 trap 'error_handler' ERR
 
 # Set up a repeating loop to send some output to Travis.
 
-bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+bash -c "while true; do echo \ $(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
-export changed_files=`git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT`
-echo $changed_files
-# Exit if nothing to check
-if [ -z "$changed_files" ]
-then
-    echo "Nothing to test"
-    kill $PING_LOOP_PID
-    exit 0
-fi
+changed_files=("boost149.rb", "sratoolkit.rb")
 
 # Check individual files ending in ".rb"
-for file in $changed_files 
+for file in "${changed_files[@]}"
 do
+    echo "Building file: $file"
     if [[ ${file: -3} == ".rb" ]]
     then
         # Dump output of building dependencies to log file
         brew reinstall $(brew deps $file) >> $BUILD_OUTPUT 2>&1
         # Explicitly print the verbose output of test-bot
-        brew test-bot $file -v
+        brew install $file -v
+        brew uninstall $file
     fi
 done
 
